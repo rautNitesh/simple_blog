12eb953087727abf14093f102ccef50364466dac
rautesh <rautesh6@gmail.com>
2020-07-22 07:51:04 +0545

    [Initial Created gitignore and env ]

---
+++ blogproject/settings/example.env.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+BASE_DIR = os.path.dirname(BASE_DIR)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
