bba375883eebf36b04cf8dfaf9e4dd32af36d7ec
Nitesh Raut <rautesh6@gmail.com>
2020-07-24 12:01:47 +0545

    [NFA : -> blog post CRUD completed]

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView
 from django.contrib.auth.decorators import login_required
+
+from .views import BlogListView, BlogCreateView, BlogPostDeleteView, BlogDetailView, BlogUpdateView
+
 
 app_name = 'blog'
 urlpatterns = [
     path('', login_required(BlogListView.as_view()), name='home'),
     path('create/', login_required(BlogCreateView.as_view()), name='create'),
+    path('delete/<int:pk>', login_required(BlogPostDeleteView.as_view()), name='delete'),
+    path('details/<slug:slug>', login_required(BlogDetailView.as_view()), name='details'),
+    path('update/<slug:slug>', login_required(BlogUpdateView.as_view()), name='update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.http import Http404, HttpResponseRedirect
 
 from .models import BlogPost
 from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
@@ -9,23 +11,51 @@ class BlogListView(ListView):
     model = BlogPost
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    queryset = BlogPost.objects.all()[::-1]
+    # queryset = BlogPost.objects.all()[::-1]
 
 
 class BlogCreateView(CreateView):
     model = BlogPost
     template_name = 'blog/create.html'
     form_class = BlogForm
+    success_url = reverse_lazy('blog:home')
 
-    def post(self, request, *args, **kwargs):
-        form = BlogForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = self.request.user
-            post.save()
-            return redirect('blog:home')
-        return render(request, 'blog/create.html', {'form': form})
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/confirm_delete.html'
+    success_url = reverse_lazy('blog:home')
+    context_object_name = 'post'
+    pk_url_kwarg = 'pk'
+
+    def dispatch(self, request, *args, **kwargs):
+        post_obj = self.get_object()
+        if post_obj.user == self.request.user:
+            return super(BlogPostDeleteView, self).dispatch(request, *args, **kwargs)
+        return Http404("This page is not available")
+
            
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/details.html'
+    context_object_name = 'post'
+
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    form_class = BlogForm
+    template_name = 'blog/update.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user == self.request.user:
+            return super().dispatch(request, *args, **kwargs)
+        raise Http404('The page you requested is not available right now or moved somewhere')
+
 
 
 
